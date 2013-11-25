06497da4ed9e3843c4e19435b60fb3f2d119e467
Alexandre Kirillov <alexandre.kirillov@gmail.com>
2013-11-25 23:05:44 +0900

    prototype: show list of users repos

--- app.go
+++ app.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"github.com/google/go-github/github"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+var (
+	userName = "marexandre"
+)
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	client := github.NewClient(nil)
+
+	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+	repos, _, err := client.Repositories.List(userName, opt)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var urls []string
+	for _, v := range repos {
+		urls = append(urls, github.Stringify(v.HTMLURL))
+	}
+	fmt.Printf("%v\n\n", urls)
+
 	t, _ := template.ParseFiles("template/index.html")
-	t.Execute(w, nil)
+	err = t.Execute(w, map[string]interface{}{"Urls": urls})
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func main() {
